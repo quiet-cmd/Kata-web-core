5c516bc199cc57e66763adca8463cf7a58ce94f7
Maxim Petrov <max.zh.petrov@gmail.com>
2022-07-18 18:33:14 +0300

    update slider

--- static/js/repair.js
+++ static/js/repair.js
@@ -4,63 +4,72 @@ function displayChanger(arr, display) {
 	}
 }
 
+let brands = document.querySelector('.brands');
+let swiper;
 
-var userWidth = document.documentElement.clientWidth;
-
-var btn = document.querySelector('.show-all-btn');
-var btnText = btn.querySelector('.show-all-btn__text');
-var btnImg = btn.querySelector('.show-all-btn__img');
-var btnChildren = [];
-
-if(userWidth <= 767) {
-	let brands = document.querySelector('.brands')
-	brands.classList.add('swiper')
-
-	const swiper = new Swiper('.swiper', {
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
-		},
-
-		slidesPerView: 1.2,
-
-		spaceBetween: 16,
-
-		slidesPerGroup: 1,
-
-		breakpoints: {
-			400: {
-				slidesPerView: 1.4
+function mobileSwiper() {
+	if(window.innerWidth <= 767 & brands.dataset.mobaile == 'false') {
+		swiper = new Swiper('.brands', {
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
 			},
+			slidesPerView: 1.2,
+			spaceBetween: 16,
+			slidesPerGroup: 1,
+			breakpoints: {
+				400: {
+					slidesPerView: 1.4
+				},
 
-			500: {
-				slidesPerView: 2.4
-			}
-		},
-	});
-} else if (768 <= userWidth & userWidth <= 1119) {
-	var btnChildren = document.querySelectorAll('.brands__brand:nth-child(6) ~ .brands__brand');
-
-} else {
-	var btnChildren = document.querySelectorAll('.brands__brand:nth-child(8) ~ .brands__brand');
+				500: {
+					slidesPerView: 2.4
+				}
+			},
+		});
+		brands.dataset.mobaile = 'true';
+	}
+	if(window.innerWidth > 767 & brands.dataset.mobaile == 'true') {
+		swiper.destroy();
+		brands.dataset.mobaile = 'false';
+	}
 }
 
-btn.addEventListener('click', function() {
+mobileSwiper();
+window.addEventListener('resize', function(event) {
+	mobileSwiper();
+}, true);
 
-	var btnSelectors = btn.classList;
 
-	if(btnSelectors.contains('show-all-btn--active')) {
-		btn.classList.remove('show-all-btn--active');
-		btnText.textContent = 'Скрыть';
-		btnImg.style.transform = 'rotate(180deg)';
-		displayChanger(btnChildren, 'flex');
+var btnShow = document.querySelector('.show-all-btn');
+var showText = btnShow.querySelector('.show-all-btn__text');
+var showImg = btnShow.querySelector('.show-all-btn__img');
+var allBrand = document.querySelectorAll('.brands__brand');
+
+
+btnShow.addEventListener('click', function() {
+
+	let btnSelectors = btnShow.classList;
+	btnShow.classList.toggle('show-all-btn--active');
+
+	if(!btnSelectors.contains('show-all-btn--active')) {
+		showText.textContent = 'Скрыть';
+		showImg.style.transform = 'rotate(180deg)';
+		displayChanger(allBrand, 'flex');
 	}
 	else {
-		btn.classList.add('show-all-btn--active');
-		btnText.textContent = 'Показать все';
-		btnImg.style.transform = 'rotate(0deg)';
-		displayChanger(btnChildren, 'none');
-	}
+		showText.textContent = 'Показать все';
+		showImg.style.transform = 'rotate(0deg)';
 
+		let userWidth = window.innerWidth;
+
+		if (768 <= userWidth & userWidth <= 1119) {
+			var hideBtn = document.querySelectorAll('.brands__device--tablet ~ .brands__brand');
+		} else {
+			var hideBtn = document.querySelectorAll('.brands__device--deck ~ .brands__brand');
+		}
+
+		displayChanger(hideBtn, 'none');
+	}
 
 })
